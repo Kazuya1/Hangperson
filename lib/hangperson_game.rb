cbd6cc7a4bd3ea208b9084943d3cc3df73b0cee8
Xinghao Cheng <xcheng4@binghamton.edu>
2017-02-03 00:18:04 +0000

    correct lib

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -2,7 +2,9 @@ class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
-
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
   # Get a word from remote "random word" service
 
   # def initialize()
@@ -10,6 +12,8 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   def self.get_random_word
@@ -18,5 +22,38 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
-
+  
+  def guess(ch)
+    if ch==nil || ch.empty? || !ch.match("[a-zA-Z]")
+      raise ArgumentError
+    end
+    ch = ch.downcase
+    if(@guesses.include?(ch) || @wrong_guesses.include?(ch))
+      return false
+    end
+    if !@word.include?(ch)
+      @wrong_guesses += ch
+    else
+      @guesses += ch
+    end
+    return true
+  end
+  
+  def word_with_guesses
+    ret = ''
+    @word.each_char do |ch|
+      ret += @guesses.include?(ch) ? ch : '-'
+    end
+    return ret
+  end
+  
+  def check_win_or_lose
+    if @wrong_guesses.length==7
+      return :lose
+    end
+    if word_with_guesses==@word
+      return :win
+    end
+    return :play
+  end
 end
